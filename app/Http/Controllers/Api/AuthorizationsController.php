a1c11c40a19515db0fe44112e5b777b8e4ee8f68
vagrant <vagrant@homestead>
2018-05-14 14:17:19 +0000

    update login api

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -31,20 +31,22 @@ class AuthorizationsController extends Controller
 
         $credentials['password'] = $request->password;
 
-        if (!$this->checkStatus($credentials)) {
-            return response(['error' => '账号已被冻结，请联系管理员。'], 400);
-        }
-
         if (!$token = \Auth::guard('api')->attempt($credentials)) {
             return response(['error' => '账号或密码错误'], 400);
         }
 
+        $user = \Auth::guard('api')->user();
+
+        if ($user['status'] !== 1) {
+            $this->logout(); // 退出登陆态
+            return response(['error' => '账号已被冻结，请联系管理员。'], 400);
+        }
         // 记录登入日志
         // event(new LoginEvent(\Auth::guard('api')->user(), new Agent(), $request->getClientIp()));
 
 
         // 使用 Auth 登录用户
-        return (new UserResource(\Auth::guard('api')->user()))->additional(['meta' => [
+        return (new UserResource($user))->additional(['meta' => [
             'access_token' => $token,
             'token_type'   => 'Bearer',
             'expires_in'   => \Auth::guard('api')->factory()->getTTL() * 60
